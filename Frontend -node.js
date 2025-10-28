1c943112b2657b086c922878bde219738302024c
magesh4441 <mageshwar0004@gmail.com>
2025-10-28 11:20:00 +0530

    Create Frontend -node.js

---
+++ Frontend -node.js
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Upload Demo</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script src="/src/App.jsx"></script>
+  </body>
+</html>
